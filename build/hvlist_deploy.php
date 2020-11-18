b0f79b2e1b930e643da41d14e03defff36556354
Jan Horacek <me@apophis.cz>
2020-11-18 13:00:08 +0100

    Separate deploy scripts for hvwww & hvslist.

--- build/hvlist_deploy.php
+++ build/hvlist_deploy.php
@@ -1,13 +1,13 @@
 <?php
 require_once("deploy-token.php");
 
-define("REMOTE_REPOSITORY", "https://github.com/kmzbrnoI/hvweb.git");
-define("DIR", "..");
-define("BRANCH", "refs/heads/master");
+define("REMOTE_REPOSITORY", "https://github.com/kmzbrnoI/hJOPserverConfig");
+define("DIR", "../hvsrepo");
+define("BRANCH", "refs/heads/mosilana-h0");
 define("LOGFILE", "/dev/null");
 define("GIT", "git");
 define("MAX_EXECUTION_TIME", 15);
 define("BEFORE_PULL", "git reset --hard @{u}");
-define("AFTER_PULL", "make all");
+define("AFTER_PULL", "cd .. && make all");
 
 require_once("git-deploy/deployer.php");
