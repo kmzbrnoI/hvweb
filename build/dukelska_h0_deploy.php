d316910cff97a6765ed70e1e39b84020fc46a30f
Jan Horacek <me@apophis.cz>
2025-01-03 23:34:33 +0100

    Use separate deplloy scripts for repositories.
    git-deploy does not work with multiple repositories at once.

---
+++ build/dukelska_h0_deploy.php
@@ -0,0 +1,14 @@
+<?php
+require_once("deploy-token.php");
+
+define("REMOTE_REPOSITORY", "https://github.com/kmzbrnoI/hJOPserverConfig");
+define("DIR", "../serverRepos/dukelska-h0");
+define("BRANCH", "refs/heads/mosilana-h0");
+define("LOGFILE", "/dev/null");
+define("GIT", "git");
+define("MAX_EXECUTION_TIME", 15);
+define("BEFORE_PULL", "git reset --hard @{u}");
+define("AFTER_PULL", "cd ../.. && make all");
+
+require_once("git-deploy/deployer.php");
+?>
---
+++ build/mendelu_tt_deploy.php
@@ -0,0 +1,14 @@
+<?php
+require_once("deploy-token.php");
+
+define("REMOTE_REPOSITORY", "https://github.com/kmzbrnoI/hJOPserverConfig");
+define("DIR", "../serverRepos/mendelu-tt");
+define("BRANCH", "refs/heads/mendelu-tt");
+define("LOGFILE", "/dev/null");
+define("GIT", "git");
+define("MAX_EXECUTION_TIME", 15);
+define("BEFORE_PULL", "git reset --hard @{u}");
+define("AFTER_PULL", "");
+
+require_once("git-deploy/deployer.php");
+?>
