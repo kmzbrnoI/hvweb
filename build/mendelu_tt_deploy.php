f2997d186f64d29638c383e13ee8913b6164d7d5
Jan Horacek <me@apophis.cz>
2025-01-03 23:38:24 +0100

    mendelu_tt_deploy: add build after pull.

--- build/mendelu_tt_deploy.php
+++ build/mendelu_tt_deploy.php
@@ -8,7 +8,7 @@ define("LOGFILE", "/dev/null");
 define("GIT", "git");
 define("MAX_EXECUTION_TIME", 15);
 define("BEFORE_PULL", "git reset --hard @{u}");
-define("AFTER_PULL", "");
+define("AFTER_PULL", "cd ../.. && make all");
 
 require_once("git-deploy/deployer.php");
 ?>
