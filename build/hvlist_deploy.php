5bd096f9f1cf5e7fc83cff24a4466d5a7ddf3154
Jan Horacek <me@apophis.cz>
2025-01-03 23:13:59 +0100

    Make only after last pull.

--- build/hvlist_deploy.php
+++ build/hvlist_deploy.php
@@ -8,7 +8,7 @@ define("LOGFILE", "/dev/null");
 define("GIT", "git");
 define("MAX_EXECUTION_TIME", 15);
 define("BEFORE_PULL", "git reset --hard @{u}");
-define("AFTER_PULL", "cd .. && make all");
+define("AFTER_PULL", "");
 
 require("git-deploy/deployer.php");
 
